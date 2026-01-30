787a535ee9f45fe2a8637139f8eedcdf3f8a3ea5
Aarya Patil <122004182+Aarya2892@users.noreply.github.com>
2026-01-30 15:16:37 +0530

    apiError.js

--- src/utils/apiError.js
+++ src/utils/apiError.js
@@ -1,3 +1,11 @@
+/*
+Error is the class we have in npm js error and we extends this class
+In our asyncHandler function we called error msg again and again so for this we create this class and
+using this we can set it according to us
+
+super keyword we used to overwrite the existing value. Here we overwrite the message.
+*/
+
 class ApiError extends Error{
     constructor(
         statusCode,
