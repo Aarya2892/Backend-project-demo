114d756bf340b57dd2bf1eb6cd055b2c2470c30d
Aarya Patil <122004182+Aarya2892@users.noreply.github.com>
2026-01-30 15:12:43 +0530

    asyncHandler.js

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,5 +1,12 @@
 // in utils we right funcions which we used repeatedly. we wrap that function and used in diff diff places
 
+/*In project we send lots of requests to DataBase and for that we can not write the function again and again so we create this 
+wrap up function and we will use this function to send the request to DB.
+
+for this we use higherOrderFunction : function who can take the function as parameter and return it.
+const asyncHandler = (fn) => async() => {}
+*/
+
 const asyncHandler = (requestHandler) =>{
    (req, res, next) =>{
     Promise.resolve(requestHandler(req, res, next))
